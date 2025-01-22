9626215fcc42f72f36484cc498fa06c8bb4907ec
Kevin Garcia Martin <kevingm@live.se>
2025-01-22 15:21:12 +0100

    Render clicked playlist with playlist cover and tracks

--- src/pages/Playlist/Playlist.jsx
+++ src/pages/Playlist/Playlist.jsx
@@ -1,32 +1,38 @@
 import "./Playlist.css";
 
-export default function Playlist() {
+export default function Playlist({ playlist }) {
+  console.log(playlist);
   return (
-    <section className="playlist">
-      <div className="playlist__head-wrapper">
-        <div className="playlist__head">
-          <div className="playlist__image-wrapper">
-            <img src="" alt="" />
-          </div>
-          <div className="playlist__info">
-            <h2>Playlist 1</h2>
-            <p>11 tracks</p>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere
-              enim debitis maxime, expedita iure ipsam officia optio quo
-              consectetur quia impedit eveniet obcaecati qui sed at. Provident
-              expedita iusto tenetur!
-            </p>
+    <section className="playlist-wrapper">
+      <div className="playlist">
+        <div className="playlist__head-wrapper">
+          <div className="playlist__head">
+            <div className="playlist__image-wrapper">
+              <img
+                src={
+                  playlist.images[1]
+                    ? playlist.images[1].url
+                    : playlist.images[0].url
+                }
+                alt="Playlist cover"
+              />
+            </div>
+            <div className="playlist__info">
+              <h2>{playlist.name}</h2>
+              <p>{playlist.tracks.total} tracks</p>
+              <p>{playlist.description}</p>
+            </div>
           </div>
         </div>
-      </div>
-      <div className="playlist__body">
-        <ul className="playlist__songs">
-          <li>Song 1</li>
-          <li>Song 2</li>
-          <li>Song 3</li>
-          <li>Song 4</li>
-        </ul>
+        <div className="playlist__body">
+          <ul className="playlist__tracks">
+            {playlist.tracks.items.map((track) => (
+              <li key={track.id}>
+                <button>{track.track.name}</button>
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
     </section>
   );
--- src/pages/Playlists/Playlists.jsx
+++ src/pages/Playlists/Playlists.jsx
@@ -2,10 +2,12 @@ import { useEffect, useState } from "react";
 import Layout from "../../Layout";
 import { api } from "../../utils/api";
 import { useAuth } from "../../hooks/useAuth";
+import Playlist from "../Playlist/Playlist";
 
 export default function Playlists() {
   const isSignedIn = useAuth();
   const [playlists, setPlaylists] = useState([]);
+  const [selectedPlaylist, setSelectedPlaylist] = useState(null);
 
   useEffect(() => {
     if (isSignedIn) {
@@ -13,13 +15,30 @@ export default function Playlists() {
     }
   }, [isSignedIn]);
 
+  const handlePlaylistClick = (playlist) => {
+    api.playlist
+      .get(playlist.id)
+      .then((playlist) => setSelectedPlaylist(playlist));
+  };
+
   return (
     <Layout>
-      <div>
-        {playlists.map((playlist) => (
-          <p key={playlist.id}>{playlist.name}</p>
-        ))}
-      </div>
+      {selectedPlaylist ? (
+        <Playlist playlist={selectedPlaylist} />
+      ) : (
+        <ul>
+          {playlists.map((playlist) => (
+            <li>
+              <button
+                key={playlist.id}
+                onClick={() => handlePlaylistClick(playlist)}
+              >
+                {playlist.name}
+              </button>
+            </li>
+          ))}
+        </ul>
+      )}
     </Layout>
   );
 }
