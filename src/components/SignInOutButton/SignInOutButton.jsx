6277b6cf42e925f08c0ec5d7afc63f1ec37256dc
Anton Hansson <125166515+antxhan@users.noreply.github.com>
2025-01-22 17:17:18 +0100

    feat: sign in and out icon

--- src/components/SignInOutButton/SignInOutButton.jsx
+++ src/components/SignInOutButton/SignInOutButton.jsx
@@ -1,15 +1,21 @@
 import "./SignInOutButton.css";
 import { useAuth } from "../../hooks/useAuth";
 import { auth } from "../../utils/auth";
+import SignInIcon from "../../icons/SignInIcon";
+import SignOutIcon from "../../icons/SignOutIcon";
 
 export default function SignInOutButton() {
   const isSignedIn = useAuth();
   return (
-    <button
-      className="sign-in-out-button"
-      onClick={isSignedIn ? () => auth.signOut() : () => auth.signIn()}
-    >
-      {isSignedIn ? "Sign Out" : "Sign In"}
-    </button>
+    <>
+      <button
+        className="sign-in-out-button"
+        onClick={isSignedIn ? () => auth.signOut() : () => auth.signIn()}
+      >
+        {/* {isSignedIn ? "Sign Out" : "Sign In"} */}
+        {isSignedIn ? <SignOutIcon /> : <SignInIcon />}
+      </button>
+      <span>{isSignedIn ? "Sign Out" : "Sign In"}</span>
+    </>
   );
 }
