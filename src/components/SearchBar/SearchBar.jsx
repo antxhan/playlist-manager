2ffa702f2d96858f2eff3dbab2bea4398745508a
Anton Hansson <125166515+antxhan@users.noreply.github.com>
2025-01-23 15:47:20 +0100

    refactor: search bar using router instead of default behaviour

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,9 +1,16 @@
 import "./SearchBar.css";
 import SearchIcon from "../../icons/SearchIcon";
+import { useNavigate } from "react-router";
 
 export default function SearchBar({ q }) {
+  let navigate = useNavigate();
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const q = e.target.q.value;
+    navigate(`/search?q=${q}`);
+  };
   return (
-    <form className="search-bar">
+    <form className="search-bar" onSubmit={handleSubmit}>
       <SearchIcon />
       <input
         type="text"
