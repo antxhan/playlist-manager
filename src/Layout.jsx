d844be9f1bc7722f9e4b3d991b9978e8f3bf0210
MaybeJod <135610495+MaybeJod@users.noreply.github.com>
2025-01-28 22:07:05 +0100

    added skeleton loader for player

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,12 +1,15 @@
 import Nav from "./components/Nav/Nav";
 import GlobalPlayer from "./components/player/GlobalPlayer";
+import { usePlayer } from "./hooks/usePlayer";
 
 export default function Layout({ children }) {
-  return (
-    <>
-      <Nav />
-      <section className="page-wrapper">{children}</section>
-      <GlobalPlayer />
-    </>
-  );
+	const player = usePlayer();
+
+	return (
+		<>
+			<Nav />
+			<section className="page-wrapper">{children}</section>
+			{player && <GlobalPlayer />}
+		</>
+	);
 }
