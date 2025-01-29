b91395c1192d16a77010a4d2ff982fc9a74c89e9
Anton Hansson <125166515+antxhan@users.noreply.github.com>
2025-01-29 13:26:46 +0100

    another icon svg prop names lol

--- src/icons/XIcon.jsx
+++ src/icons/XIcon.jsx
@@ -7,10 +7,10 @@ export default function XIcon() {
       viewBox="0 0 24 24"
       fill="none"
       stroke="currentColor"
-      stroke-width="2"
-      stroke-linecap="round"
-      stroke-linejoin="round"
-      class="icon icon-tabler icons-tabler-outline icon-tabler-x"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      className="icon icon-tabler icons-tabler-outline icon-tabler-x"
     >
       <path stroke="none" d="M0 0h24v24H0z" fill="none" />
       <path d="M18 6l-12 12" />
