49c17881b0dbeafe76227f607a4f28d5cfb1dc21
Kevin Garcia Martin <kevingm@live.se>
2025-01-31 11:36:35 +0100

    Import error page css and change class names

--- src/pages/NotFound/NotFound.jsx
+++ src/pages/NotFound/NotFound.jsx
@@ -1,27 +1,28 @@
 import "./NotFound.css";
+import "../ErrorPage/ErrorPage.css";
 import { NavLink } from "react-router";
 import Layout from "../../Layout";
 import AccentButton from "../../components/buttons/AccentButton/AccentButton";
 
 export default function NotFound() {
-	return (
-		<Layout>
-			<div className="notFound-wrapper">
-				<p className="notFound--big404-wrapper">
-					<span className="notFound--big404">404</span>
-				</p>
-				<div className="notFound__info-wrapper">
-					<h1>
-						The page you are looking for is either a deep cut... or it doesn't
-						exist
-					</h1>
+  return (
+    <Layout>
+      <div className="error-wrapper">
+        <p className="error__status-code-wrapper">
+          <span className="error__status-code">404</span>
+        </p>
+        <div className="error__info-wrapper">
+          <h1>
+            The page you are looking for is either a deep cut... or it doesn't
+            exist
+          </h1>
 
-					<p>Maybe you can find what you need back at home</p>
-					<NavLink to="/" className="notFound--cta">
-						<AccentButton>Back to Home</AccentButton>
-					</NavLink>
-				</div>
-			</div>
-		</Layout>
-	);
+          <p>Maybe you can find what you need back at home</p>
+        </div>
+        <NavLink to="/" className="error__cta">
+          <AccentButton>Back to Home</AccentButton>
+        </NavLink>
+      </div>
+    </Layout>
+  );
 }
