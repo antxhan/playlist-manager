dfb698bff5b08196d14b00bb1e31ec265f4843ce
Kevin Garcia Martin <kevingm@live.se>
2025-02-02 15:05:22 +0100

    Refactor dialog to increase accessibility

--- src/components/dialogs/Dialog/Dialog.jsx
+++ src/components/dialogs/Dialog/Dialog.jsx
@@ -1,21 +1,37 @@
 import "./Dialog.css";
+import React, { useRef, useEffect } from "react";
+import ReactDOM from "react-dom";
 import StandardButton from "../../buttons/StandardButton/StandardButton";
 import XIcon from "../../../icons/XIcon";
 
 export default function Dialog({ isOpen, onClose, title, children }) {
+  const dialogRef = useRef(null);
+
+  useEffect(() => {
+    const dialog = dialogRef.current;
+    if (dialog) {
+      if (isOpen) {
+        dialog.showModal();
+      } else {
+        dialog.close();
+      }
+    }
+  }, [isOpen]);
+
   if (!isOpen) return null;
 
-  return (
-    <dialog open className="dialog">
+  return ReactDOM.createPortal(
+    <dialog ref={dialogRef} className="dialog" onClose={onClose}>
       <div className="dialog__content">
         <header className="dialog__header">
           <h3>{title}</h3>
-          <StandardButton onClick={onClose} className={'small-btn'}>
+          <StandardButton onClick={onClose} className={"small-btn"}>
             <XIcon />
           </StandardButton>
         </header>
         <main className="dialog__body">{children}</main>
       </div>
-    </dialog>
+    </dialog>,
+    document.body
   );
 }
