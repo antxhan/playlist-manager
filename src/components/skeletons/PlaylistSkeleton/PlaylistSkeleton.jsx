50c4f48d81e85ae8ccc369f231f0f3acab557024
Kevin Garcia Martin <kevingm@live.se>
2025-02-03 10:17:46 +0100

    Add skeleton to info group and expand description skeleton

--- src/components/skeletons/PlaylistSkeleton/PlaylistSkeleton.jsx
+++ src/components/skeletons/PlaylistSkeleton/PlaylistSkeleton.jsx
@@ -9,7 +9,17 @@ export default function PlaylistSkeleton() {
             <div className="playlist__image-wrapper playlist-skeleton__image skeleton"></div>
             <div className="playlist__info playlist-skeleton__info">
               <div className="playlist-skeleton__title skeleton"></div>
-              <div className="playlist-skeleton__description skeleton"></div>
+              <div className="playlist__info-group playlist-skeleton__info-group">
+                <span className="skeleton"></span>
+                <span className="skeleton"></span>
+              </div>
+              <div className="playlist-skeleton__description">
+                <span className="skeleton"></span>
+                <span className="skeleton"></span>
+                <span className="skeleton"></span>
+                <span className="skeleton"></span>
+                <span className="skeleton"></span>
+              </div>
             </div>
           </div>
         </div>
