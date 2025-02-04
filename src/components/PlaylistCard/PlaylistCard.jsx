49db34e65b2f4dfb43ec56667f25b0b291adb481
Kevin Garcia Martin <kevingm@live.se>
2025-02-04 10:57:00 +0100

    Add fade in animation on render

--- src/components/PlaylistCard/PlaylistCard.jsx
+++ src/components/PlaylistCard/PlaylistCard.jsx
@@ -1,5 +1,6 @@
 import "./PlaylistCard.css";
 import { Link } from "react-router-dom";
+import { motion } from "framer-motion";
 import { useNavigateWithTransition } from "../../hooks/useNavigateWithTransition";
 import placeholderImage from "../../img/placeholder.webp";
 
@@ -9,42 +10,48 @@ export default function PlaylistCard({ playlist }) {
   if (!playlist) return null;
 
   return (
-    <Link
-      to={{
-        pathname: `/playlists/${playlist.id}`,
-        state: { transition: true },
-      }}
-      className="playlist-card"
-      onClick={(e) => {
-        e.preventDefault();
-        navigateWithTransition(`/playlists/${playlist.id}`);
-      }}
+    <motion.div
+      initial={{ opacity: 0, y: 20 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ duration: 0.5}}
     >
-      <img
-        src={
-          playlist.images && playlist.images[0]
-            ? playlist.images[0].url
-            : placeholderImage
-        }
-        alt="Playlist cover"
-        width={150}
-        loading="lazy"
-      />
-      <div className="playlist-card__info">
-        <p>{playlist.name || "Unknown Playlist"}</p>
-        <p>
-          {playlist.owner
-            ? playlist.owner.display_name
+      <Link
+        to={{
+          pathname: `/playlists/${playlist.id}`,
+          state: { transition: true },
+        }}
+        className="playlist-card"
+        onClick={(e) => {
+          e.preventDefault();
+          navigateWithTransition(`/playlists/${playlist.id}`);
+        }}
+      >
+        <img
+          src={
+            playlist.images && playlist.images[0]
+              ? playlist.images[0].url
+              : placeholderImage
+          }
+          alt="Playlist cover"
+          width={150}
+          loading="lazy"
+        />
+        <div className="playlist-card__info">
+          <p>{playlist.name || "Unknown Playlist"}</p>
+          <p>
+            {playlist.owner
               ? playlist.owner.display_name
-              : playlist.owner.id
-            : "Unknown owner"}
-        </p>
-        <p>
-          {playlist.tracks.total > 0
-            ? `${playlist.tracks.total} tracks`
-            : "No tracks"}
-        </p>
-      </div>
-    </Link>
+                ? playlist.owner.display_name
+                : playlist.owner.id
+              : "Unknown owner"}
+          </p>
+          <p>
+            {playlist.tracks.total > 0
+              ? `${playlist.tracks.total} tracks`
+              : "No tracks"}
+          </p>
+        </div>
+      </Link>
+    </motion.div>
   );
 }
