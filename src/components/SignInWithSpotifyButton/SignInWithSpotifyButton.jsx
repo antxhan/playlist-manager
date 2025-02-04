055b228772379aaa3356c671b7352b0afae99348
Kevin Garcia Martin <kevingm@live.se>
2025-02-04 17:15:10 +0100

    Add standard button component

--- src/components/SignInWithSpotifyButton/SignInWithSpotifyButton.jsx
+++ src/components/SignInWithSpotifyButton/SignInWithSpotifyButton.jsx
@@ -1,14 +1,15 @@
 import "./SignInWithSpotifyButton.css";
 import { auth } from "../../utils/auth";
+import StandardButton from "../buttons/StandardButton/StandardButton";
 
 export default function SignInWithSpotifyButton() {
   return (
-    <button
+    <StandardButton
       className="sign-in-with-spotify-button"
       onClick={() => auth.signIn()}
     >
       <span>Sign in with</span>
       <img src="/images/spotify.svg" alt="spotify" width={128} />
-    </button>
+    </StandardButton>
   );
 }
