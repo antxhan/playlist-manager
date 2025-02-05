76c8b9fab2cf062627253d2df769a34ae8158d28
MaybeJod <135610495+MaybeJod@users.noreply.github.com>
2025-02-05 11:42:23 +0100

    fix: set maximum 300 characters of description in edit/add modal

--- src/components/dialogs/PlaylistDialogs/CreatePlaylistDialog/CreatePlaylistDialog.jsx
+++ src/components/dialogs/PlaylistDialogs/CreatePlaylistDialog/CreatePlaylistDialog.jsx
@@ -4,55 +4,62 @@ import { useState } from "react";
 import Dialog from "../../Dialog/Dialog";
 
 export default function CreatePlaylistDialog({
-  isOpen,
-  onClose,
-  onSubmit,
-  title,
-  children,
+	isOpen,
+	onClose,
+	onSubmit,
+	title,
+	children,
 }) {
-  const [name, setName] = useState("");
-  const [description, setDescription] = useState("");
-  const [addTopTracks, setAddTopTracks] = useState(false);
+	const DESCRIPTION_LIMIT = 300;
+	const [name, setName] = useState("");
+	const [description, setDescription] = useState("");
+	const [addTopTracks, setAddTopTracks] = useState(false);
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    onSubmit(name, description, addTopTracks);
-    setName("");
-    setDescription("");
-    setAddTopTracks(false);
-    onClose();
-  };
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		onSubmit(name, description, addTopTracks);
+		setName("");
+		setDescription("");
+		setAddTopTracks(false);
+		onClose();
+	};
 
-  return (
-    <Dialog isOpen={isOpen} onClose={onClose} title={title}>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-          placeholder="Playlist name (required)"
-          aria-label="Playlist name"
-          required
-        />
-        <textarea
-          value={description}
-          onChange={(e) => setDescription(e.target.value)}
-          placeholder="Playlist description"
-          aria-label="Playlist description"
-        ></textarea>
-        <div className="playlist-dialog__checkbox-wrapper">
-          <input
-            type="checkbox"
-            id="addTopTracks"
-            checked={addTopTracks}
-            onChange={(e) => setAddTopTracks(e.target.checked)}
-          />
-          <label htmlFor="addTopTracks">
-            Add your top 20 tracks
-          </label>
-        </div>
-        <div className="dialog__button-wrapper">{children}</div>
-      </form>
-    </Dialog>
-  );
+	return (
+		<Dialog isOpen={isOpen} onClose={onClose} title={title}>
+			<form onSubmit={handleSubmit}>
+				<input
+					type="text"
+					value={name}
+					onChange={(e) => setName(e.target.value)}
+					placeholder="Playlist name (required)"
+					aria-label="Playlist name"
+					required
+				/>
+				<textarea
+					value={description}
+					onChange={(e) => setDescription(e.target.value)}
+					placeholder="Playlist description"
+					aria-label="Playlist description"
+					maxLength={DESCRIPTION_LIMIT}></textarea>
+				<div className="create-playlist-counter-label">
+					<span className="create-playlist-counter-text">
+						{description.length}
+					</span>
+					<span className="create-playlist-counter-limit">
+						/{DESCRIPTION_LIMIT}
+					</span>
+				</div>
+				<div className="playlist-dialog__checkbox-wrapper">
+					<input
+						type="checkbox"
+						id="addTopTracks"
+						checked={addTopTracks}
+						onChange={(e) => setAddTopTracks(e.target.checked)}
+					/>
+					<label htmlFor="addTopTracks">Add your top 20 tracks</label>
+				</div>
+				<div className="dialog__button-wrapper">{children}</div>
+			</form>
+		</Dialog>
+	);
 }
--- src/components/dialogs/PlaylistDialogs/EditPlaylistDialog/EditPlaylistDialog.jsx
+++ src/components/dialogs/PlaylistDialogs/EditPlaylistDialog/EditPlaylistDialog.jsx
@@ -4,42 +4,51 @@ import { useState } from "react";
 import Dialog from "../../Dialog/Dialog";
 
 export default function PlaylistDialog({
-  isOpen,
-  onClose,
-  onSubmit,
-  title,
-  children,
-  initialName,
-  initialDescription,
+	isOpen,
+	onClose,
+	onSubmit,
+	title,
+	children,
+	initialName,
+	initialDescription,
 }) {
-  const [name, setName] = useState(initialName);
-  const [description, setDescription] = useState(initialDescription);
+	const DESCRIPTION_LIMIT = 300;
+	const [name, setName] = useState(initialName);
+	const [description, setDescription] = useState(initialDescription);
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    onSubmit(name, description);
-    onClose();
-  };
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		onSubmit(name, description);
+		onClose();
+	};
 
-  return (
-    <Dialog isOpen={isOpen} onClose={onClose} title={title}>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-          placeholder="Playlist name (required)"
-          aria-label="Playlist name"
-          required
-        />
-        <textarea
-          value={description}
-          onChange={(e) => setDescription(e.target.value)}
-          placeholder="Playlist description"
-          aria-label="Playlist description"
-        ></textarea>
-        <div className="dialog__button-wrapper">{children}</div>
-      </form>
-    </Dialog>
-  );
+	return (
+		<Dialog isOpen={isOpen} onClose={onClose} title={title}>
+			<form onSubmit={handleSubmit}>
+				<input
+					type="text"
+					value={name}
+					onChange={(e) => setName(e.target.value)}
+					placeholder="Playlist name (required)"
+					aria-label="Playlist name"
+					required
+				/>
+				<textarea
+					value={description}
+					onChange={(e) => setDescription(e.target.value)}
+					placeholder="Playlist description"
+					aria-label="Playlist description"
+					maxLength={DESCRIPTION_LIMIT}></textarea>
+				<div className="edit-playlist-counter-label">
+					<span className="edit-playlist-counter-text">
+						{description.length}
+					</span>
+					<span className="edit-playlist-counter-limit">
+						/{DESCRIPTION_LIMIT}
+					</span>
+				</div>
+				<div className="dialog__button-wrapper">{children}</div>
+			</form>
+		</Dialog>
+	);
 }
