bfb13b4bd2a82ee1c7fc276f64bd2b00dfd921f2
Anton Hansson <125166515+antxhan@users.noreply.github.com>
2025-02-05 11:44:14 +0100

    fix: prevent grid end message showing before results load in

--- src/pages/Search/Search.jsx
+++ src/pages/Search/Search.jsx
@@ -13,18 +13,18 @@ const InfinitePlaylistGrid = lazy(() =>
 export default function Search() {
   let navigate = useNavigate();
   const [searchResults, setSearchResults] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
+  // const [isLoading, setIsLoading] = useState(false);
   const [nextPage, setNextPage] = useState(null);
   const [searchParams, setSearchParams] = useSearchParams();
   const q = searchParams.get("q");
 
   useEffect(() => {
     if (q) {
-      setIsLoading(true);
+      // setIsLoading(true);
       api.search({ q }).then((results) => {
         setSearchResults(results.playlists.items);
         setNextPage(results.playlists.next);
-        setIsLoading(false);
+        // setIsLoading(false);
       });
     }
   }, [q]);
@@ -53,16 +53,18 @@ export default function Search() {
         <SearchBar q={searchParams.get("q")} onSubmit={handleSearch} />
       </header>
       <main>
-        {q || searchResults.length > 0 ? (
-          <Suspense fallback={<InfinitePlaylistGridSkeleton />}>
-            <InfinitePlaylistGrid
-              playlists={searchResults.filter((res) => !!res)}
-              getNextPage={getNextPage}
-              hasMore={nextPage}
-            />
-          </Suspense>
-        ) : isLoading ? (
-          <InfinitePlaylistGridSkeleton />
+        {q ? (
+          searchResults.length > 0 ? (
+            <Suspense fallback={<InfinitePlaylistGridSkeleton />}>
+              <InfinitePlaylistGrid
+                playlists={searchResults.filter((res) => !!res)}
+                getNextPage={getNextPage}
+                hasMore={nextPage}
+              />
+            </Suspense>
+          ) : (
+            <InfinitePlaylistGridSkeleton />
+          )
         ) : (
           <RecommendView />
         )}
