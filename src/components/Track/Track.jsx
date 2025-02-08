8f290c0773cea3373054195d14c851c4f84ad010
Kevin Garcia Martin <kevingm@live.se>
2025-02-08 16:13:14 +0100

    Add play button and change track name and artist into links

--- src/components/Track/Track.jsx
+++ src/components/Track/Track.jsx
@@ -1,4 +1,5 @@
 import "./Track.css";
+import { Link } from "react-router-dom";
 import { easeInOut, motion } from "framer-motion";
 import { msToMMSS } from "../../utils/utils";
 import missingAlbumCover from "../../img/placeholder.webp";
@@ -6,8 +7,7 @@ import missingAlbumCover from "../../img/placeholder.webp";
 export default function Track({ track, onClick, index }) {
   return (
     <>
-      <motion.button
-        onClick={onClick}
+      <motion.div
         initial={{ opacity: 0, x: 20 }}
         animate={{ opacity: 1, x: 0 }}
         transition={{
@@ -18,23 +18,52 @@ export default function Track({ track, onClick, index }) {
       >
         {!track ? null : (
           <div className="track">
+            <button onClick={onClick}>Play</button>
             <img
               src={track?.album?.images?.[2]?.url || missingAlbumCover}
               alt="Track album cover"
             />
             <div className="track__info">
-              <p className="track__name">{track?.name || "Unknown Track"}</p>
-              <p className="track__artists">
-                {track?.artists.map((artist) => artist.name).join(", ") ||
-                  "Unknown Artist"}
-              </p>
+              <Link
+                to={`https://open.spotify.com/track/${track.id}`}
+                target="_blank"
+              >
+                <p className="track__name">{track?.name || "Unknown Track"}</p>
+              </Link>
+              {track.artists ? (
+                <div className="track__artists">
+                  {track.artists.map((artist, index, array) => (
+                    <Link
+                      to={`https://open.spotify.com/artist/${artist.id}`}
+                      target="_blank"
+                    >
+                      <p>
+                        {index === array.length - 1
+                          ? artist.name
+                          : `${artist.name},`}
+                      </p>
+                    </Link>
+                  ))}
+                </div>
+              ) : (
+                <p>Unknown Artist</p>
+              )}
+              {/* <Link
+                to={`https://open.spotify.com/artist/0JeTRYMH7FoBiMcLXg1n8g`}
+                target="_blank"
+              >
+                <p className="track__artists">
+                  {track?.artists.map((artist) => artist.name).join(", ") ||
+                    "Unknown Artist"}
+                </p>
+              </Link> */}
             </div>
             <p className="track__duration">
               {track.duration_ms ? msToMMSS(track.duration_ms) : "--:--"}
             </p>
           </div>
         )}
-      </motion.button>
+      </motion.div>
     </>
   );
 }
